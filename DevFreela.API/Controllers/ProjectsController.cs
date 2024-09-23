1f806caa4f38718fbdad3e34bd98f4c8db703b20
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-09-23 19:18:09 -0300

    Initial commit

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,48 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetId(int id)
+        {
+            //return not found()
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Description.Length > 50)
+            {
+                return BadRequest();
+            }
+            return CreatedAtAction(nameof(GetId), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 50)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
