227026909a9b696f68797a56938c24d3de664844
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-09-24 08:26:39 -0300

    Utilizando o IFormfile para enviar arquivos através da API

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -22,5 +22,13 @@ namespace DevFreela.API.Controllers
             return Ok();
         }
 
+        [HttpPut]
+        public IActionResult PostProfilePicture(IFormFile file) {
+
+            var description = $"File: {file.FileName}, Size: {file.Length}";
+            //processa a imagem
+            return Ok(description);
+        }
+
     }
 }
