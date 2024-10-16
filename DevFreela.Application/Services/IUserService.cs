751ff8d35f9ea5fb542e20a64ae73ef26dfd30ae
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-10-15 22:15:23 -0300

    Adicionando a interface User Service

---
+++ DevFreela.Application/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services
+{
+    public interface IUserService
+    {
+        ResultViewModel<ProjectViewModel> GetById(int id);
+
+        ResultViewModel<ProjectViewModel> Insert(CreateUserInputModel model);
+        ResultViewModel<ProjectViewModel> Delete(int id);
+        ResultViewModel<ProjectViewModel> Update(CreateUserInputModel model);
+        ResultViewModel<ProjectViewModel> PostSkills(int id, UserSkillsInputModel model);
+    }
+}
