a10a31d7250acaa2d184e3251f0826be6690ad1a
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-10-25 20:39:43 -0300

    Incluindo padrão Decorator com MediatR

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -57,6 +57,10 @@ namespace DevFreela.API.Controllers
         {
             var result = await _mediator.Send(command);
 
+            if (!result.IsSuccess) {
+                return BadRequest(result.Message);
+            }
+
             return CreatedAtAction(nameof(GetById), new { id = result.Data}, command);
         }
 
