285b31cfc2b6506ac418e810fc4826e2698e9bd1
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-11-18 22:20:26 -0300

    Add Fluent Validator in project

--- DevFreela.Application/ApplicationModule.cs
+++ DevFreela.Application/ApplicationModule.cs
@@ -1,5 +1,7 @@
 ﻿using DevFreela.Application.Commands.InsertProject;
 using DevFreela.Application.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,7 +12,8 @@ namespace DevFreela.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services
-                .AddHandlers();
+                .AddHandlers()
+                .AddValidation();
 
             return services;
         }
@@ -21,6 +24,15 @@ namespace DevFreela.Application
             config.RegisterServicesFromAssemblyContaining<InsertProjectCommand>());
 
             services.AddTransient<IPipelineBehavior<InsertProjectCommand, ResultViewModel<int>>, ValidateInsertProjectCommandBehavior>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            services
+                .AddFluentValidationAutoValidation()
+                .AddValidatorsFromAssemblyContaining<InsertProjectCommand>();
 
             return services;
         }
