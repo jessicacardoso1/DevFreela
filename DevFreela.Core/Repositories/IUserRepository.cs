70825c36c2d4364d72d3e618be94a837ed6ed06a
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-11-19 22:45:28 -0300

    Create User Repository.

---
+++ DevFreela.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAll();
+        Task<User?> GetById(int id);
+        Task<int> Add(User user);
+        //Task<int> PostSkills(int id, UserSkill userSkill);
+        Task Update(User user);
+        Task<bool> Exists(int id);
+    }
+}
---
+++ DevFreela.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿
+using Azure.Core;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DevFreelaDbContext _context;
+        public UserRepository(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Add(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user.Id;
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+           return await _context.Users.AnyAsync(x => x.Id == id);
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            var users = await _context.Users
+              .Include(u => u.Skills)
+                 .ThenInclude(u => u.Skill)
+              .Where(u => !u.IsDeleted).ToListAsync();
+
+            return users;
+        }
+
+        public async Task<User?> GetById(int id)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == id);
+
+            return user;
+        }
+
+        //public Task<int> PostSkills(int id, UserSkill userSkill)
+        //{
+        //    //TODO
+        //    //var userSkils = _context.SkillIds.Select(s => new UserSkill(userSkill.Id, s)).ToList();
+        //    //await _context.AddRangeAsync(userSkils);
+        //    //await _context.SaveChangesAsync();
+        //}
+
+        public async Task Update(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
