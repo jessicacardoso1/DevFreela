be58f45922a331750d34a4bdfb28c732e309e5f4
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-11-23 20:56:55 -0300

    removing context and mediator

--- DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
+++ DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
@@ -13,22 +13,18 @@ namespace DevFreela.Application.Commands.InsertProject
 {
     public class InsertProjectHandler : IRequestHandler<InsertProjectCommand, ResultViewModel<int>>
     {
-        private readonly DevFreelaDbContext _context;
-        private readonly IMediator _mediator;
         private readonly IProjectRepository _repository;
-        public InsertProjectHandler(DevFreelaDbContext context, IMediator mediator, IProjectRepository repository)
+        public InsertProjectHandler(IProjectRepository repository)
         {
             _repository = repository;
-            _context = context;
-            _mediator = mediator;
         }
         public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
         {
             var project = request.ToEntity();
             await _repository.Add(project);
 
-            var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
-            await _mediator.Publish(projectCreated);
+            //var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
+            //await _mediator.Publish(projectCreated);
 
             return ResultViewModel<int>.Success(project.Id);
         }
