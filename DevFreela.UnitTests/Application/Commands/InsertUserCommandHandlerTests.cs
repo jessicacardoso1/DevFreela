f4e9978254e74f58d0c8e81ec046ea34b4aedd19
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-11-28 06:24:16 -0300

    Create InsertUserCommandHandlerTests.cs

---
+++ DevFreela.UnitTests/Application/Commands/InsertUserCommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using DevFreela.Application.Commands.InsertUser;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class InsertUserCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnUserId()
+        {
+            // Arrange
+            var userRepository = new Mock<IUserRepository>();
+
+            var insertUserCommand = new InsertUserCommand
+            {
+                FullName = "Jéssica",
+                Email = "jessica.cardosor@gmail.com",
+                BirthDate = DateTime.Now
+            };
+
+            var insertUserCommandHandler = new InsertUserHandler(userRepository.Object);
+
+            // Act
+            var id = await insertUserCommandHandler.Handle(insertUserCommand, new CancellationToken());
+
+            // Assert
+            Assert.True(id.Data >= 0);
+
+            userRepository.Verify(u => u.Add(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
