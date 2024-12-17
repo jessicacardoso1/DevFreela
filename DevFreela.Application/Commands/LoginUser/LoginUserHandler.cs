b189a53667a95debfd6ce61ebe74621fa5be726e
Jéssica Cardoso <jessica.cardosor@gmail.com>
2024-12-16 22:06:11 -0300

    Criação dos command e handler para Login

---
+++ DevFreela.Application/Commands/LoginUser/LoginUserHandler.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.LoginUser
+{
+    public class LoginUserHandler : IRequestHandler<LoginUserCommand, ResultViewModel<LoginUserViewModel>>
+    {
+        private readonly IAuthService _authService;
+        private readonly IUserRepository _userRepository;
+        public LoginUserHandler(IAuthService authService, IUserRepository userRepository)
+        {
+            _authService = authService;
+            _userRepository = userRepository;
+        }
+        public async Task<ResultViewModel<LoginUserViewModel>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            var user = await _userRepository.GetUserByEmailAndPassordAsync(request.Email, passwordHash);
+            if (user == null)
+            {
+                return ResultViewModel<LoginUserViewModel>.Error("Usuario não existe");
+
+            }
+            var token = _authService.GenerateJwtToken(user.Email, user.Role);
+            return ResultViewModel<LoginUserViewModel>.Success(new LoginUserViewModel(user.Email, token));
+
+        }
+    }
+}
