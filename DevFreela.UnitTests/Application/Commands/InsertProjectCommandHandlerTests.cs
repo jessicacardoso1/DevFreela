bc9d2106b470059c2e941a33e0d16dbced52c0c2
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-05-20 07:50:53 -0300

    Adding NSubstitute

--- DevFreela.UnitTests/Application/Commands/InsertProjectCommandHandlerTests.cs
+++ DevFreela.UnitTests/Application/Commands/InsertProjectCommandHandlerTests.cs
@@ -1,7 +1,9 @@
 ﻿using DevFreela.Application.Commands.InsertProject;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using IdentityModel.OidcClient;
 using Moq;
+using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -37,6 +39,35 @@ namespace DevFreela.UnitTests.Application.Commands
             Assert.True(id.Data >= 0);
 
             projectRepository.Verify(pr => pr.Add(It.IsAny<Project>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId_NSubstitute()
+        {
+            // Arrange
+            const int ID = 1;
+            var projectRepository = Substitute.For<IProjectRepository>();
+            projectRepository.Add(Arg.Any<Project>()).Returns(Task.FromResult(ID));
+
+            var createProjectCommand = new InsertProjectCommand
+            {
+                Title = "Titulo de Teste",
+                Description = "Uma descrição Daora",
+                TotalCost = 50000,
+                IdClient = 1,
+                IdFreelancer = 2
+            };
+
+            var createProjectCommandHandler = new InsertProjectHandler(projectRepository);
+
+            // Act
+            var result = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(ID, result.Data);
+
+            projectRepository.Received(1).Add(Arg.Any<Project>());
         }
     }
 }
